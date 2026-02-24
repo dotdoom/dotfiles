096cc4c04d3ae1c0aeacb4f61c72a56587291c82
Artem Sheremet <dot.doom@gmail.com>
2026-02-24 20:33:11 +0100

    Remap keys on German keyboard

--- flake.nix
+++ flake.nix
@@ -58,6 +58,29 @@
 
               nixpkgs.config.allowUnfree = true;
               programs.vscode.enable = true;
+
+              launchd.agents.keyboard-remap = {
+                # Remap top-left key (paragraph) to backquote and backslash like
+                # proper ISO keyboard does, and the key right to the LShift to
+                # Shift.
+                enable = true;
+                config = {
+                  Label = "com.user.keyboard-remap";
+                  ProgramArguments = [
+                    "/usr/bin/hidutil"
+                    "property"
+                    "--set"
+                    ''
+                      {"UserKeyMapping":
+                        [
+                          {"HIDKeyboardModifierMappingSrc":0x700000035, "HIDKeyboardModifierMappingDst":0x7000000e1},
+                          {"HIDKeyboardModifierMappingSrc":0x700000064, "HIDKeyboardModifierMappingDst":0x700000035},
+                        ]
+                      }''
+                  ];
+                  RunAtLoad = true;
+                };
+              };
             }
           )
         ];
