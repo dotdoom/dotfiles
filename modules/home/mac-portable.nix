e733c3d86d1e3155c53e53e6ec975de82ca31d68
Artem Sheremet <dot.doom@gmail.com>
2026-04-20 19:25:36 +0200

    Migrate to Secretive fully

--- modules/home/mac-portable.nix
+++ modules/home/mac-portable.nix
@@ -12,8 +12,11 @@
   ];
 
   programs.zsh.envExtra = ''
-    # Blocked on https://github.com/overhacked/ssh-agent-mux/issues/56
-    # export SSH_AUTH_SOCK=~/.ssh/ssh-agent-mux.sock
+    # If Secretive doesn't recognize your Yubikey PIV, it's possible you
+    # generated it using yubikey-agent and that did not update CHUID. Simply
+    # running 'ykman piv objects generate chuid' should be sufficient.
+    # https://github.com/maxgoedjen/secretive/issues/333
+
     export SSH_AUTH_SOCK=~/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
   '';
 
