91507f173dceb26226c3e5badd80323bc8660746
Artem Sheremet <dot.doom@gmail.com>
2026-07-12 13:39:59 +0200

    Bring agy back, remove annoying remap

--- hosts/mars/darwin.nix
+++ hosts/mars/darwin.nix
@@ -1,6 +1,9 @@
 _: {
   homebrew.casks = [
     "bambu-studio"
+
+    # Not available in nixpkgs-26.05, and latest doesn't support x86_64-darwin
+    "antigravity-cli"
   ];
   homebrew.brews = [
     "libimobiledevice"
